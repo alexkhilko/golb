0da7ab7de4360c9afd8eeb51e6d23980153d6344
Alex Khilko <alex.khilko@tripledotstudios.com>
2024-06-26 16:56:53 +0200

    Implement healthchecks

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"io"
 	"fmt"
 	"flag"
+	"net/url"
 )
 
 var (
 	counter int
 	servers []string
 	client *http.Client
+	heathCheckInterval int
 )
 
 
@@ -51,13 +53,52 @@ func redirectToServer(w http.ResponseWriter, r *http.Request) {
 	io.Copy(w, resp.Body)
 }
 
+func healthCheckServers(initialServers []string) {
+	client = &http.Client{
+        Timeout: 1 * time.Second, 
+    }
+	for {
+		healthyServers := []string{}
+		for _, server := range initialServers {
+			resp, err := client.Get(server)
+			if err != nil {
+				fmt.Printf("failed to check health of %s with %s\n", server, err)
+				continue
+			} 
+			if resp.StatusCode != http.StatusOK {
+				fmt.Printf("healthcheck failed for %s with %d\n", server, resp.StatusCode)
+				continue
+			}
+			healthyServers = append(healthyServers, server)
+		}
+		servers = healthyServers
+		time.Sleep(time.Duration(heathCheckInterval) * time.Second)
+	}
+}
+
+func validateServerURLs(urls []string) []string {
+	if urls == nil {
+		return []string{}
+	}
+	validUrls := []string{}
+	for _, server := range urls {
+		_, err := url.ParseRequestURI(server)
+		if err != nil {
+		   fmt.Println("Incorrect url", server)
+		   continue
+		}
+		validUrls = append(validUrls, server)
+	}
+	return validUrls
+}
+
 
 func main() {
+	flag.IntVar(&heathCheckInterval, "h", 5, "Interval between health checks in seconds")
 	flag.Parse()
-	servers = flag.Args()
-	if servers == nil {
-		servers = []string{}
-	}
+	servers = validateServerURLs(flag.Args())
+	go healthCheckServers(servers)
+
 	client = &http.Client{
         Timeout: 5 * time.Second, 
     }
